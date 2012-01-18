74bc277e57f824f0c3fe0e103a6cc67127f498d9
Tomas Krajci <suicidaldj@gmail.com>
2012-01-18 06:20:49 +0100

    Modified project settings

--- Lands/src/main/java/org/cellcode/lands/Lands.java
+++ Lands/src/main/java/org/cellcode/lands/Lands.java
@@ -1,17 +1,30 @@
 package org.cellcode.lands;
 
+import java.util.logging.Logger;
+
+import org.bukkit.event.Event;
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Lands extends JavaPlugin {
 
+	public static Lands plugin;
+	public final Logger logger = Logger.getLogger("Minecraft");
+
 	public void onDisable() {
-		// TODO Auto-generated method stub
-		
+
+		PluginDescriptionFile pdFile = this.getDescription();
+		this.logger.info(pdFile.getName() + " disabled. ");
+
 	}
 
 	public void onEnable() {
-		// TODO Auto-generated method stub
-		
+		PluginManager pm = getServer().getPluginManager();
+		PluginDescriptionFile pdFile = this.getDescription();
+		this.logger.info(pdFile.getName() + " version " + pdFile.getVersion()
+				+ " enabled. ");
+
 	}
 
 }
