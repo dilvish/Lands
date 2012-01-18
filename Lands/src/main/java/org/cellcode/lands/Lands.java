316555544ffd3e942f20d8a179b5614b9ddf3b5e
Tomas Krajci <suicidaldj@gmail.com>
2012-01-18 06:16:37 +0100

    First class

---
+++ Lands/src/main/java/org/cellcode/lands/Lands.java
@@ -0,0 +1,17 @@
+package org.cellcode.lands;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Lands extends JavaPlugin {
+
+	public void onDisable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onEnable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
