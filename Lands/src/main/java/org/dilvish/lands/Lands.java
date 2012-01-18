2a434d019cb81ea46d83dc03dde4aba92e8ff7a1
Tomas Krajci <suicidaldj@gmail.com>
2012-01-18 18:49:28 +0100

    Pay no attention

--- Lands/src/main/java/org/dilvish/lands/Lands.java
+++ Lands/src/main/java/org/dilvish/lands/Lands.java
@@ -6,6 +6,7 @@ import org.bukkit.event.Event;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.dilvish.lands.listeners.LandsPlayerListener;
 
 public class Lands extends JavaPlugin {
 
@@ -21,6 +22,8 @@ public class Lands extends JavaPlugin {
 
 	public void onEnable() {
 		PluginManager pm = getServer().getPluginManager();
+		pm.registerEvent(Event.Type.PLAYER_MOVE, new LandsPlayerListener(this), Event.Priority.Normal, this);
+		
 		PluginDescriptionFile pdFile = this.getDescription();
 		this.logger.info(pdFile.getName() + " version " + pdFile.getVersion()
 				+ " enabled. ");
---
+++ Lands/src/main/java/org/dilvish/lands/listeners/LandsPlayerListener.java
@@ -0,0 +1,28 @@
+package org.dilvish.lands.listeners;
+
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.dilvish.lands.Lands;
+
+public class LandsPlayerListener extends PlayerListener {
+	
+	public Lands plugin;
+	
+	public LandsPlayerListener(Lands plugin) {
+		this.plugin = plugin;
+	}
+	
+    public void onPlayerMove(PlayerMoveEvent evt) {
+    	Location loc = evt.getPlayer().getLocation();
+    	World w = loc.getWorld();
+    	loc.setY(loc.getY() + 5);
+    	Block b = w.getBlockAt(loc);
+    	b.setTypeId(1);
+    }
+
+
+
+}
